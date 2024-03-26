27ecf9c89a0de689cd77f46f4df7f8fd0ca8c966
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-03-26 16:27:03 +0000

    Add purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
+import postcssImport from 'postcss-import'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import purgecss from '@fullhuman/postcss-purgecss'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    postcssImport,
+    tailwindcss,
+    autoprefixer,
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          purgecss({
+            content: ['./index.html', './src/**/*.tsx', './pages/**/*.tsx'],
+            defaultExtractor: (content) =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || [], // Specify the default extractor for PurgeCSS
+          }),
+        ]
+      : []),
+  ],
 }
