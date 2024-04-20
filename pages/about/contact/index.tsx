8543363bf5a9583eecb588d1b4c1dd95d60c4989
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-04-20 17:29:33 +0100

    Slightly update contact page; delete unused contact pages

--- pages/about/contact/index.tsx
+++ pages/about/contact/index.tsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import Layout from '../../layout'
 
-const Contact = () => {
-  return (
-    <Layout>
-      Contact
-      <img
-        src="/images/project-logos/white-bg.png"
-        alt="FULFIL logo"
-        height={75}
-        width={75}
-      />
-    </Layout>
-  )
+const Contact = ({ children }) => {
+  return <Layout>{children}</Layout>
 }
 
 export default Contact
