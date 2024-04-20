abd161316a15ba84605a418b29bfa3fb4930b504
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-04-20 16:24:23 +0100

    Add legal and privacy notice; start adding footer

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import './index.css'
+import { Link } from 'react-router-dom'
 
 const Layout = ({ children }) => {
   useEffect(() => {
@@ -11,7 +12,19 @@ const Layout = ({ children }) => {
     }
   }, [document])
 
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      <div className="absolute bg-gray-300 h-[200px] w-screen ml-[-40px] bottom-0 px-10 pb-10">
+        <div className="flex gap-x-10 absolute bottom-10">
+          <div>Copyright ©2024 FULFIL</div>
+          <Link to="/legal-and-privacy" className="underline">
+            Legal and Privacy Information
+          </Link>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Layout
