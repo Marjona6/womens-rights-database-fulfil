bc47974a41921b7f10a050cd86dd83d44a95d8a5
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-04-20 17:29:56 +0100

    Small improvements to partners and project pages

--- pages/about/partners/index.tsx
+++ pages/about/partners/index.tsx
@@ -2,18 +2,7 @@ import React from 'react'
 import Layout from '../../layout'
 
 const Partners = ({ children }) => {
-  return (
-    <Layout>
-      Partners
-      <img
-        src="/images/project-logos/white-bg.png"
-        alt="FULFIL logo"
-        height={75}
-        width={75}
-      />
-      {children}
-    </Layout>
-  )
+  return <Layout>{children}</Layout>
 }
 
 export default Partners
--- pages/about/project/index.tsx
+++ pages/about/project/index.tsx
@@ -4,13 +4,6 @@ import Layout from '../../layout'
 const Project = ({ children }) => {
   return (
     <Layout>
-      About the project
-      <img
-        src="/images/project-logos/white-bg.png"
-        alt="FULFIL logo"
-        height={75}
-        width={75}
-      />
       <div className="!list-disc">{children}</div>
     </Layout>
   )
