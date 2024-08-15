746b16917500444b91b1b920a8433b46249e8015
Meryem Yanar <meryem.yanaryanar@gmail.com>
2024-08-15 15:08:00 +0100

    Add a nice pretty purple button to "Submit a case" section

--- pages/rights/resources/index.tsx
+++ pages/rights/resources/index.tsx
@@ -6,8 +6,24 @@ const Resources = ({ children }) => {
   return (
     <Layout>
       <h1 className="text-[32px] font-semibold mb-4">Resources</h1>
+      Click the name of any fact sheet to view it.
       <ThumbnailGallery />
       {children}
+      <p>
+        Note: The following survey aims to gather case law related to migrant
+        women’s fundamental rights violation in the EU. All answers are optional
+        and cases can cover different jurisdictions.
+      </p>
+      <div className="flex justify-center m-4">
+        <a
+          href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__QntMuVURjQ5SDk3OTNQS01UUVpXVFI0N09QN1dYMS4u"
+          target="_blank"
+        >
+          <button className="bg-purple text-white px-4 py-2 rounded-full hover:bg-lightpurple">
+            Submit a Case
+          </button>
+        </a>
+      </div>
     </Layout>
   )
 }
